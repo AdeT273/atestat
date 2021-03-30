c8afbc7ee0b6cbaf3c0268f799de7ca49c398317
AdeT27 <44448305+AdeT27@users.noreply.github.com>
2021-03-30 08:33:09 +0300

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,206 @@
+﻿namespace creca
+{
+    partial class Romania
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Romania));
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.Maramureș = new System.Windows.Forms.Button();
+            this.Crișana = new System.Windows.Forms.Button();
+            this.Transilvania = new System.Windows.Forms.Button();
+            this.Banat = new System.Windows.Forms.Button();
+            this.Oltenia = new System.Windows.Forms.Button();
+            this.Bucovina = new System.Windows.Forms.Button();
+            this.Moldova = new System.Windows.Forms.Button();
+            this.Muntenia = new System.Windows.Forms.Button();
+            this.Dobrogea = new System.Windows.Forms.Button();
+            this.button10 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.Color.Transparent;
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(146, 36);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(874, 445);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
+            // 
+            // Maramureș
+            // 
+            this.Maramureș.BackColor = System.Drawing.Color.Transparent;
+            this.Maramureș.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.Maramureș.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.Maramureș.Location = new System.Drawing.Point(12, 91);
+            this.Maramureș.Name = "Maramureș";
+            this.Maramureș.Size = new System.Drawing.Size(128, 34);
+            this.Maramureș.TabIndex = 1;
+            this.Maramureș.Text = "Maremureș";
+            this.Maramureș.UseVisualStyleBackColor = false;
+            this.Maramureș.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Crișana
+            // 
+            this.Crișana.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.Crișana.Location = new System.Drawing.Point(12, 171);
+            this.Crișana.Name = "Crișana";
+            this.Crișana.Size = new System.Drawing.Size(128, 34);
+            this.Crișana.TabIndex = 2;
+            this.Crișana.Text = "Crișana";
+            this.Crișana.UseVisualStyleBackColor = true;
+            this.Crișana.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Transilvania
+            // 
+            this.Transilvania.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.Transilvania.Location = new System.Drawing.Point(12, 251);
+            this.Transilvania.Name = "Transilvania";
+            this.Transilvania.Size = new System.Drawing.Size(128, 34);
+            this.Transilvania.TabIndex = 3;
+            this.Transilvania.Text = "Transilvania";
+            this.Transilvania.UseVisualStyleBackColor = true;
+            // 
+            // Banat
+            // 
+            this.Banat.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.Banat.Location = new System.Drawing.Point(12, 331);
+            this.Banat.Name = "Banat";
+            this.Banat.Size = new System.Drawing.Size(128, 34);
+            this.Banat.TabIndex = 4;
+            this.Banat.Text = "Banat";
+            this.Banat.UseVisualStyleBackColor = true;
+            // 
+            // Oltenia
+            // 
+            this.Oltenia.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.Oltenia.Location = new System.Drawing.Point(12, 411);
+            this.Oltenia.Name = "Oltenia";
+            this.Oltenia.Size = new System.Drawing.Size(128, 34);
+            this.Oltenia.TabIndex = 5;
+            this.Oltenia.Text = "Oltenia";
+            this.Oltenia.UseVisualStyleBackColor = true;
+            // 
+            // Bucovina
+            // 
+            this.Bucovina.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.Bucovina.Location = new System.Drawing.Point(1018, 91);
+            this.Bucovina.Name = "Bucovina";
+            this.Bucovina.Size = new System.Drawing.Size(128, 34);
+            this.Bucovina.TabIndex = 6;
+            this.Bucovina.Text = "Bucovina";
+            this.Bucovina.UseVisualStyleBackColor = true;
+            // 
+            // Moldova
+            // 
+            this.Moldova.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.Moldova.Location = new System.Drawing.Point(1018, 171);
+            this.Moldova.Name = "Moldova";
+            this.Moldova.Size = new System.Drawing.Size(128, 34);
+            this.Moldova.TabIndex = 7;
+            this.Moldova.Text = "Moldova";
+            this.Moldova.UseVisualStyleBackColor = true;
+            // 
+            // Muntenia
+            // 
+            this.Muntenia.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.Muntenia.Location = new System.Drawing.Point(1018, 251);
+            this.Muntenia.Name = "Muntenia";
+            this.Muntenia.Size = new System.Drawing.Size(128, 34);
+            this.Muntenia.TabIndex = 8;
+            this.Muntenia.Text = "Muntenia";
+            this.Muntenia.UseVisualStyleBackColor = true;
+            // 
+            // Dobrogea
+            // 
+            this.Dobrogea.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.Dobrogea.Location = new System.Drawing.Point(1018, 331);
+            this.Dobrogea.Name = "Dobrogea";
+            this.Dobrogea.Size = new System.Drawing.Size(128, 34);
+            this.Dobrogea.TabIndex = 9;
+            this.Dobrogea.Text = "Dobrogea";
+            this.Dobrogea.UseVisualStyleBackColor = true;
+            // 
+            // button10
+            // 
+            this.button10.BackColor = System.Drawing.Color.Red;
+            this.button10.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.button10.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.button10.Location = new System.Drawing.Point(1026, 447);
+            this.button10.Name = "button10";
+            this.button10.Size = new System.Drawing.Size(120, 34);
+            this.button10.TabIndex = 10;
+            this.button10.Text = "Ieșire";
+            this.button10.UseVisualStyleBackColor = false;
+            // 
+            // Romania
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoValidate = System.Windows.Forms.AutoValidate.EnablePreventFocusChange;
+            this.BackColor = System.Drawing.SystemColors.InactiveBorder;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(1158, 510);
+            this.Controls.Add(this.button10);
+            this.Controls.Add(this.Dobrogea);
+            this.Controls.Add(this.Muntenia);
+            this.Controls.Add(this.Moldova);
+            this.Controls.Add(this.Bucovina);
+            this.Controls.Add(this.Oltenia);
+            this.Controls.Add(this.Banat);
+            this.Controls.Add(this.Transilvania);
+            this.Controls.Add(this.Crișana);
+            this.Controls.Add(this.Maramureș);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Romania";
+            this.Text = "România";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button Maramureș;
+        private System.Windows.Forms.Button Crișana;
+        private System.Windows.Forms.Button Transilvania;
+        private System.Windows.Forms.Button Banat;
+        private System.Windows.Forms.Button Oltenia;
+        private System.Windows.Forms.Button Bucovina;
+        private System.Windows.Forms.Button Moldova;
+        private System.Windows.Forms.Button Muntenia;
+        private System.Windows.Forms.Button Dobrogea;
+        private System.Windows.Forms.Button button10;
+    }
+}
+
